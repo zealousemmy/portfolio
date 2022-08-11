c371d521622153be708bc2cfe257ae36fd640f05
Emmyzealous <emmygazzillion123@gail.com>
2022-08-11 10:29:48 +0100

    portfolio_one

---
+++ components/layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+
+import Header from "./header";
+import Footer from "./footer";
+
+interface LayoutProps {
+  children?: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col h-screen justify-between font-content antialiased">
+      {/* <Header /> */}
+      <main className="mb-auto">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
